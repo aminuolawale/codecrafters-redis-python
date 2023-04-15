3823f0b56f94f002d9e14e412d9a7bd57e286fd9
mohammedaminu <aminuolawalekan@gmail.com>
2023-04-15 13:52:19 +0200

    update

--- app/main.py
+++ app/main.py
@@ -3,16 +3,12 @@ import socket
 
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
-
-    # Uncomment this to pass the first stage
-    #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     while True:
         connection, address = server_socket.accept()  # wait for client
         try:
-            connection.recv(1024).decode()
+            data = connection.recv(1024).decode()
             connection.send("+PONG\r\n".encode())
         except ConnectionResetError:
             pass
